3517b7d3bc59f53a337fd2a12096c9b17ff0292d
misael dessalegn <misaeldessalegn@gmail.com>
2023-05-03 12:40:46 +0300

    save all

---
+++ src/Data/Data.jsx
@@ -0,0 +1,27 @@
+import card from '../Asset/card.png'
+import card1 from '../Asset/card1.png'
+import card2 from '../Asset/card2.png'
+const data={
+    news:[
+        {title:'Our City',desc:"You never know about gamo city and culture is here",img:card},
+        {title:'Code to Success ',desc:'Don’t lose your hope the secrete to success in our campus is?',img:card1},
+        {title:'Next Opportunity ',desc:'Get Scholarship to day  open for every one ',img:card2},
+    ],
+    Gcal:[
+        {value:'A+ 95-100'},
+        {value:'A 90-95'},
+        {value:'A- 85-90'},
+        {value:'B+ 95-100'},
+        {value:'B 90-95'},
+        {value:'B- 85-90'},
+        {value:'C+ 95-100'},
+        {value:'C 90-95'},
+        {value:'A- 85-90'},
+        {value:'D+ 95-100'},
+        {value:'A 90-95'},
+        {value:'A- 85-90'},
+        {value:"F '<'35"},
+    ],
+   
+}
+export default data;
